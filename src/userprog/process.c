bbfe21c2a05398ae6525bcfbf3c26e087e3f73bb
josh24 <josh24@vt.edu>
2024-02-24 20:46:45 -0500

    removed an unused tag

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -136,7 +136,7 @@ start_process (void *file_name_)
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) 
+process_wait (tid_t child_tid) 
 {
   // struct list_elem *e;
   struct thread *cur = thread_current();
