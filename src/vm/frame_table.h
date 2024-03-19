08694423c88e4cd27dbaeb933429650d13fec065
daniel20 <daniel20@vt.edu>
2024-03-19 14:50:22 -0400

    added mmap and unmmap declarations to frame interface, added alignment macro

--- src/vm/frame_table.h
+++ src/vm/frame_table.h
@@ -1,3 +1,6 @@
+#include "vaddr.h"
+// macro to verify page alignment
+#define aligned(addr)  ((addr) % PGSIZE == 0)
 // gets a free frame and returns phys addr of free frame
 // evicts if necessary
 void* get_frame(void* v_addr);
@@ -9,3 +12,7 @@ void evict();
 void move_to_swap();
 // moves frame from swap partition, frees slot
 void move_from_swap();
+
+// run use frame table to manage file mappings with mmap
+void frame_mmap();
+void frame_unmmap();
