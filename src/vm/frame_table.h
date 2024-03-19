c369e9dd2908b8d8e8ebd35bb60ad2896348ab56
daniel20 <daniel20@vt.edu>
2024-03-19 14:43:56 -0400

    added swap partition modification functions

--- src/vm/frame_table.h
+++ src/vm/frame_table.h
@@ -3,3 +3,9 @@
 void* get_frame(void* v_addr);
 // helper function for eviction
 void evict();
+
+// moves frame to swap partition
+// panic if no slots available
+void move_to_swap();
+// moves frame from swap partition, frees slot
+void move_from_swap();
