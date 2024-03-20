3dcd3f93b1352cac7ed4ef6febc0dba67a5f58ed
daniel20 <daniel20@vt.edu>
2024-03-20 12:30:04 -0400

    added frame files

---
+++ src/vm/frame.h
@@ -0,0 +1,31 @@
+#include "vaddr.h"
+#include "list.h"
+// macro to verify page alignment
+#define aligned(addr)  ((addr) % PGSIZE == 0)
+
+
+struct frame_table_entry {
+    void *physical_addr;
+    struct page_table_entry *resident;
+    struct list_elem elem;
+};
+struct spinlock frame_table_lock; // TODO: look at turning this into a per-frame lock
+static struct list frame_table;
+
+// gets a free frame and returns frame_table_entry corresponding to the selected frame
+// evicts if necessary
+struct frame_table_entry* get_frame(void* v_addr);
+
+// helper function for eviction
+// returns frame_table entry
+void* evict();
+
+// moves frame to swap partition
+// panic if no slots available
+void move_to_swap();
+// moves frame from swap partition, frees slot
+void move_from_swap();
+
+// run use frame table to manage file mappings with mmap
+void frame_mmap();
+void frame_unmmap();
